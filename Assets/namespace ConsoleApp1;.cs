d91e1acd4669cdc51e2bac902964d77f96e47230
M-sakito <sakitiizu@gmail.com>
2025-07-14 17:51:31 +0900

    プログラムを試しに作りました

---
+++ Assets/namespace ConsoleApp1;.cs
@@ -0,0 +1,19 @@
+namespace ConsoleApp1;
+
+main()
+{
+    int point = 0;
+    if (point > 0)
+    {
+        MessageBox.Show("Enterキーが押されました");
+    }
+    if (e.Key == Key.Enter)
+    {
+        point++;
+    }
+}
+
+public class Class1
+{
+    
+}
